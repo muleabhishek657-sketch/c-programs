fe75296c33456297773bf1e79bd2f22297d6835e
muleabhishek657-sketch <muleabhishek657@gmail.com>
2026-09-19 10:20:26 +0530

    Add files via upload

---
+++ square.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("Enter the number to be squarred:");
+    scanf("%d",&a);
+    int b=a*a;
+    printf("The square of the number is: %d",b);
+    
+
+}
